ad3a2f904db69232785abfff04f55f17a7cc3da1
Kenan Teeboon <20467@burnside.school.nz>
2023-05-11 12:45:55 +1200

    hello

--- Website year project/app/routes.py
+++ Website year project/app/routes.py
@@ -11,6 +11,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/food")
+def food():
+    return render_template("food.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
